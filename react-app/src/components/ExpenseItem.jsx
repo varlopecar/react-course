5712285e39d4e166e8b0d64c609646ba93779410
Carlos Vargas <varlopecar@gmail.com>
2022-03-12 21:51:45 -0600

    Adding styles

--- react-app/src/components/ExpenseItem.jsx
+++ react-app/src/components/ExpenseItem.jsx
@@ -1,16 +1,13 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 3, 12);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 297.12;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
